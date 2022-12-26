21b36f1ee97e887b53194d2260df662bc85dc533
Hadarios <nolan.djellal@gmail.com>
2022-12-26 01:34:55 +0100

    j'ai 3 de QI

--- DL from Song List UI JSON.py
+++ DL from Song List UI JSON.py
@@ -19,7 +19,7 @@ while not source:
         print("Please input the path to a JSON file.")
 
 dir = filename[0:-5] + " downloads"
-dirs = input("Download folder (default:" + dir + "\) : " if os.name == "nt" else "/) : ").replace("\"","")
+dirs = input("Download folder (default: " + dir + "\) : " if os.name == "nt" else "/) : ").replace("\"","")
 dir = dirs if dirs.strip() != "" else dir
 
 print()
@@ -33,46 +33,45 @@ progress = 0
 
 for L in source:
     url = L['urls']['catbox']['0']
-    if url:
-        if ann != str(L['annId']):
-            print("Fetching next ANN entry")
-            ann = str(L['annId'])
-            r = requests.get("https://www.animenewsnetwork.com/encyclopedia/anime.php?id=" + ann)
-            soup = BeautifulSoup(r.content, 'html.parser')
-            anime = soup.select_one('#page_header').text.strip()
-            print("Fetched " + anime)
-            ins = 1
+    if ann != str(L['annId']):
+        print("Fetching next ANN entry")
+        ann = str(L['annId'])
+        r = requests.get("https://www.animenewsnetwork.com/encyclopedia/anime.php?id=" + ann)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        anime = soup.select_one('#page_header').text.strip()
+        print("Fetched " + anime)
+        ins = 1
 
-        print("Downloading " + L['name'] + " from catbox.moe")
-        response = requests.get(url)
-        if L['type'][0] == "I":
-            type = "Insert " + str(ins)
-            ins+=1
-        else:
-            type = L['type']
-        print("Complete")
+    print("Downloading " + L['name'] + " from catbox.moe")
+    response = requests.get(url)
+    if L['type'][0] == "I":
+        type = "Insert " + str(ins)
+        ins+=1
+    else:
+        type = L['type']
+    print("Complete")
 
-        print("Writing on file")
-        name = anime + " - " + type + " (" + L['name'] + " by " + L['artist'] + ")"
-        namefile = ""
-        for i in range(len(name)):
-            if name[i] not in ["/", "\\", "*", "|", ":", "?", "\"", "<", ">"]:
-                namefile += name[i]
-        namefile = dir + "\\" + namefile + ".mp3"
-        open(namefile, "wb").write(response.content)
-        print("Written")
+    print("Writing on file")
+    name = anime + " - " + type + " (" + L['name'] + " by " + L['artist'] + ")"
+    namefile = ""
+    for i in range(len(name)):
+        if name[i] not in ["/", "\\", "*", "|", ":", "?", "\"", "<", ">"]:
+            namefile += name[i]
+    namefile = dir + "\\" + namefile + ".mp3"
+    open(namefile, "wb").write(response.content)
+    print("Written")
 
-        print('Adding metadata')
-        audiofile = eyed3.load(namefile)
-        if not audiofile.tag:
-            audiofile.initTag()
+    print('Adding metadata')
+    audiofile = eyed3.load(namefile)
+    if not audiofile.tag:
+        audiofile.initTag()
 
-        audiofile.tag.artist = L['artist']
-        audiofile.tag.title = L['name'] + " (" + anime + " - " + type + ")"
-        audiofile.tag.save()
-        print("Added")
-        progress+=1
-        print("{0} complete, to next song! {1}/{2} ({3}%)\n".format(name, progress, len(source), round(progress/len(source)*100), 2))
+    audiofile.tag.artist = L['artist']
+    audiofile.tag.title = L['name'] + " (" + anime + " - " + type + ")"
+    audiofile.tag.save()
+    print("Added")
+    progress+=1
+    print("{0} complete, to next song! {1}/{2} ({3}%)\n".format(name, progress, len(source), round(progress/len(source)*100), 2))
 
 
 input("Done! Press Enter to exit.")
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# DL-from-anisongdb-JSON
-A program to download all the songs from an anisongdb.com JSON file as mp3 files with title and artist ID3 tags.  
+# DL-from-Song-List-UI-JSON
+A program to download all the songs from Song List UI by TheJoseph98 JSON file as mp3 files with title and artist ID3 tags.  
 Python depedencies: requests, BeautifulSoup, eyed3
 The .exe should work standalone. If you have any problem or question, feel free to contact me.
 
